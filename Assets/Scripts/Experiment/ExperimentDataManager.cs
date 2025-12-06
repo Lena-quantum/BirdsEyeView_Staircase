f05d6c712e506fa1809c2e1b7602ae5c218f08de
niklassindemann-ctrl <niklas.sindemann@gmail.com>
2025-12-06 17:42:13 +0000

    Fixed saving architechture and B button

--- Assets/Scripts/Experiment/ExperimentDataManager.cs
+++ Assets/Scripts/Experiment/ExperimentDataManager.cs
@@ -487,7 +487,8 @@ namespace Experiment
         private string GetSaveFolderPath()
         {
             string basePath = Application.persistentDataPath;
-            string experimentFolder = Path.Combine(basePath, "isometric_corridor");
+            string experimentFolder = Path.Combine(basePath, "BirdsEyeViewStaircase");
+            Debug.Log($"[ExperimentDataManager] Save folder path: {experimentFolder}");
             return experimentFolder;
         }
 
